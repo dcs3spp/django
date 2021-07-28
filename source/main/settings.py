33a67c5a9b91c2a2e1abd2e6ae1a4a91c74d6a66
dcs3spp <simonppears@googlemail.com>
2021-07-28 17:33:43 +0100

    fix: authentication urls are now accessible
    added django_debug_toolbar to installed apps and middlewares if DEBUG is true

--- source/main/settings.py
+++ source/main/settings.py
@@ -59,15 +59,46 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+if DEBUG:
+    print("Enabling the debug toolbar")
+    INTERNAL_IPS = ('127.0.0.1', 'localhost',)
+    MIDDLEWARE += (
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+    )
+
+    INSTALLED_APPS += (
+        'debug_toolbar',
+    )
+
+    DEBUG_TOOLBAR_PANELS = [
+        'debug_toolbar.panels.versions.VersionsPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        'debug_toolbar.panels.settings.SettingsPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.signals.SignalsPanel',
+        'debug_toolbar.panels.logging.LoggingPanel',
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+    ]
+
+    DEBUG_TOOLBAR_CONFIG = {
+        'INTERCEPT_REDIRECTS': False,
+    }
+
 ROOT_URLCONF = 'main.urls'
-# LOGIN_REDIRECT_URL = "home"   # Route defined in pages/urls.py
-# LOGOUT_REDIRECT_URL = "home"  # Route defined in pages/urls.py
+LOGIN_REDIRECT_URL = "home"   # Route defined in pages/urls.py
+LOGOUT_REDIRECT_URL = "home"  # Route defined in pages/urls.py
 TEMPLATE_DIR = os.path.join(CORE_DIR, "main/templates")  # ROOT dir for templates
+ACCOUNTS_DIR = os.path.join(CORE_DIR, "main/templates/accounts")
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR],
+        'DIRS': [TEMPLATE_DIR, ACCOUNTS_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- source/main/urls.py
+++ source/main/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
@@ -20,7 +21,12 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("local.api.urls")),
-    path("", include("local.pages.urls")),
     path("", include("local.authentication.urls")),
+    path("", include("local.pages.urls")),
 ]
 
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path(r"__debug__/", include(debug_toolbar.urls)),
+    ]
